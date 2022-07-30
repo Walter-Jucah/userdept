bb0a457b9468884b142b8f4dadee20f4a899eeb4
Walter-Jucah <52893282+Walter-Jucah@users.noreply.github.com>
2022-07-29 23:24:15 -0300

    first commit

---
+++ src/main/java/com/devsuperior/userdept/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.devsuperior.userdept.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
