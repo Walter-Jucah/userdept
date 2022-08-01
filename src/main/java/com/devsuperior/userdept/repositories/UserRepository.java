708ab921003bd32d46ae44ed3dbe6fd16fb2e025
Walter-Jucah <rune.walsh@gmail.com>
2022-08-01 13:42:51 -0300

    projeto finalizado

--- src/main/java/com/devsuperior/userdept/repositories/UserRepository.java
+++ src/main/java/com/devsuperior/userdept/repositories/UserRepository.java
@@ -2,6 +2,8 @@ package com.devsuperior.userdept.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.devsuperior.userdept.entities.User;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
